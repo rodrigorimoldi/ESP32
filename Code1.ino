e7e69a28434247259881961a495a0627a46ab27c
Rodrigo Rimoldi de Lima <rodrigo.rimoldi@gmail.com>
2024-07-31 14:20:05 -0300

    Update Code1.ino

--- Code1.ino
+++ Code1.ino
@@ -1,6 +1,16 @@
+/*
+ * Arquivo:     Code1.ino
+ * Autor:       Prof. Rodrigo Rimoldi de Lima
+ *
+ * Criacao:     30/07/2024
+ * 
+ * Descricao:   Codigo desenvolvido para piscar um LED e desenvolver o
+ *              primeiro contato com a programacao do ESP32.
+ */
+
 // Definições de variáveis e constantes
-const int ledPin    = 15;    // GPI15
-const int timer     = 1000;
+#define ledPin  2     // GPIO2
+#define timer   1000  // Tempo definido
 
 // Parametrizações do dispositivo
 void setup() {
@@ -8,6 +18,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);  // Saída digital
 }
 
+//Loop infinito
 void loop() {
   // Liga o LED
   digitalWrite(ledPin, HIGH);
